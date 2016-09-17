fcf442de52365f33329bc771e819181208f6bf24
Ashish Negi <ashish@quintype.com>
2016-09-17 18:36:39 +0530

    Generates word count of a website.

---
+++ src/Sanskell/Words.hs
@@ -0,0 +1,46 @@
+module Sanskell.Words (wordCloudOfWebsite) where
+
+import qualified Network.URI as NU
+import qualified Control.Concurrent as Con
+import qualified Sanskell.Crawler as SC
+import qualified Sanskell.Types as ST
+import qualified Data.Text as T
+import qualified Data.Map as M
+import qualified Data.Foldable as DF
+import qualified Data.Char as DC
+
+
+wordCloudOfWebsite :: String -> ST.JobId -> IO (Either String (M.Map T.Text Integer))
+wordCloudOfWebsite url jobId = do
+  texts <- textsOnWebsite url jobId
+  return $ wordCount <$> texts
+
+textsOnWebsite :: String -> ST.JobId -> IO (Either String [ T.Text ])
+textsOnWebsite url jobId = do
+  let parsedUri = NU.parseURI url
+
+  maybe (return $ Left "Parsing of url failed") (\ uri -> do
+      let config = ST.CrawlConfig 3
+          link = ST.Link jobId 3 uri
+
+      crawlResultChan <- Con.newChan
+      SC.crawlingThread config link crawlResultChan
+
+      allTexts <- foldr (joinAllTexts crawlResultChan) (return []) [1..]
+      return $ Right allTexts)
+    parsedUri
+
+  where
+    joinAllTexts crawlResultChan _ t = do
+      texts <- t
+      m <- Con.readChan crawlResultChan
+      case m of
+        ST.CrawlFinished -> return texts
+        ST.CrawlResult _ _ text -> return $ (T.concat text) : texts
+
+wordCount :: [ T.Text ] -> M.Map T.Text Integer
+wordCount texts = DF.foldl' countWords M.empty texts
+  where
+    countWords m text =
+      let words = T.split DC.isSeparator text
+      in DF.foldl' (\m' word -> M.insertWith (+) word 1 m') m words
