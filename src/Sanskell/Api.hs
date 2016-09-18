12c798fab831842980566dafd3ef041e64e8df34
Ashish Negi <ashish@quintype.com>
2016-09-18 18:18:23 +0530

    sends 404 if /job/:id is not present or some error occurred during async operation.

--- src/Sanskell/Api.hs
+++ src/Sanskell/Api.hs
@@ -9,6 +9,8 @@ import qualified Servant as S
 import Servant ((:>), (:<|>))
 import qualified Data.Aeson as A
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Map as M
 import qualified Sanskell.Types as ST
 import qualified Sanskell.Server as SS
@@ -16,7 +18,7 @@ import qualified Control.Monad.IO.Class as CM
 
 import Debug.Trace
 
-type JobApi = "job" :> S.Capture "id" ST.JobId :> S.Get '[S.JSON] (Either T.Text ST.JobResult )
+type JobApi = "job" :> S.Capture "id" ST.JobId :> S.Get '[S.JSON] ST.JobResult
          :<|> "job" :> S.ReqBody '[S.JSON] ST.JobPostBody :> S.Post '[S.JSON] (Either T.Text ST.JobId)
 
 jobApi :: S.Proxy JobApi
@@ -26,8 +28,12 @@ serverRouter :: ST.Server -> S.Server JobApi
 serverRouter server = jobGet
     S.:<|> jobPost
   where
-    jobGet :: ST.JobId -> S.Handler (Either T.Text ST.JobResult)
-    jobGet id = CM.liftIO $ SS.jobResult server id
+    jobGet :: ST.JobId -> S.Handler ST.JobResult
+    jobGet id = do
+      res <- CM.liftIO $ SS.jobResult server id
+      case res of
+        Left e -> S.throwError (S.err404 { S.errBody = TLE.encodeUtf8 . TL.fromStrict $ e })
+        Right v -> return v
 
     jobPost :: ST.JobPostBody -> S.Handler (Either T.Text ST.JobId)
     jobPost ST.JobPostBody{..} = traceShow jobUrl $ CM.liftIO $ SS.addJob server jobUrl
